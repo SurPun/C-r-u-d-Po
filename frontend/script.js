bc4b635f585eea92af593037cda1065913f7f7d0
Suraj <S_pun@outlook.com>
2023-05-15 18:02:21 +0100

    Added Frontend

---
+++ frontend/script.js
@@ -0,0 +1,24 @@
+const apiUrl = 'https://rvv24kxgo7.execute-api.eu-west-2.amazonaws.com/Prod/user';
+
+async function fetchItems() {
+  try {
+    const response = await fetch(apiUrl);
+    const items = await response.json();
+    let itemsHtml = '';
+
+    items.forEach((item) => {
+      itemsHtml += 
+        `<p>
+            Email: ${item.UserId}<br>
+            Name: ${item.FirstName} ${item.LastName}
+        </p>
+        `;
+    });
+
+    document.getElementById('items').innerHTML = itemsHtml;
+  } catch (error) {
+    console.error('Error fetching items:', error);
+  }
+}
+
+fetchItems();
