7256c51e5cd4117f010c57ff7927a0023fc5e6c1
Suraj <S_pun@outlook.com>
2023-05-15 17:19:16 +0100

    Created API

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -52,3 +52,46 @@ resource "aws_iam_role_policy_attachment" "lambda_exec" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
   role       = aws_iam_role.lambda_exec.name
 }
+
+// API Gateway
+resource "aws_api_gateway_rest_api" "user_api" {
+  name        = "user-api"
+  description = "Employee API"
+}
+
+resource "aws_api_gateway_resource" "user_resource" {
+  rest_api_id = aws_api_gateway_rest_api.user_api.id
+  parent_id   = aws_api_gateway_rest_api.user_api.root_resource_id
+  path_part   = "user"
+}
+
+resource "aws_api_gateway_method" "user_method" {
+  rest_api_id   = aws_api_gateway_rest_api.user_api.id
+  resource_id   = aws_api_gateway_resource.user_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "user_integration" {
+  rest_api_id = aws_api_gateway_rest_api.user_api.id
+  resource_id = aws_api_gateway_resource.user_resource.id
+  http_method = aws_api_gateway_method.user_method.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.user_lambda.invoke_arn
+}
+
+resource "aws_lambda_permission" "allow_api_gateway" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.user_lambda.arn
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.user_api.execution_arn}/*/*/user"
+}
+
+resource "aws_api_gateway_deployment" "user_deployment" {
+  depends_on  = [aws_api_gateway_integration.user_integration]
+  rest_api_id = aws_api_gateway_rest_api.user_api.id
+  stage_name  = "dev"
+}
