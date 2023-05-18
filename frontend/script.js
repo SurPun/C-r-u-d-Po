d8bd8fedcfaaa1aeaeb3cd583f1a564ad31fca5a
Suraj <S_pun@outlook.com>
2023-05-18 16:28:27 +0100

    Put Html

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,45 @@
 const apiUrl = 'https://91mzwuihk7.execute-api.eu-west-2.amazonaws.com/dev/user';
 
+// CREATE
+document.getElementById('user-form').addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const email = document.getElementById('email').value;
+  const firstName = document.getElementById('first-name').value;
+  const lastName = document.getElementById('last-name').value;
+  const github = document.getElementById('github').value;
+
+  const requestOptions = {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      Email: email,
+      FirstName: firstName,
+      LastName: lastName,
+      GitHub: github,
+    }),
+  };
+
+  try {
+    const response = await fetch(apiUrl, requestOptions);
+    const data = await response.json();
+    console.log(data);
+
+    if (response.status === 200) {
+      alert('Successfully submitted data');
+    } else {
+      alert('Error submitting data');
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    alert('Error submitting data');
+  }
+});
+
+
+// READ 
 async function fetchItems() {
   try {
     const response = await fetch(apiUrl);
@@ -33,3 +73,7 @@ async function fetchItems() {
 }
 
 fetchItems();
+
+// UPDATE
+
+// DELETE
